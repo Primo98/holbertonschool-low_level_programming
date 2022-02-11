9600a6a3d58be3436b4e3bdbc0968456e11b4cb7
Diego <granadosdieguito1@gmail.com>
2022-02-10 16:52:00 -0800

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  * main - print message to console
+  *
+  * Return: a 0 (zero) is returned when executing the script
+  */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
+}
